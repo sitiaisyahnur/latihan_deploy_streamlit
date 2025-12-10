099f17ca434f4bc1d146066bd8bb37d0b7526a2d
sitiaisyahnur <siti10222084@sttcipasung.ac.id>
2025-12-10 11:14:28 +0700

    upload pertama

---
+++ app.py
@@ -0,0 +1,24 @@
+import joblib
+import streamlit as st
+
+model = joblib.load(r"models\model_logistic_regression.pkl")
+tfidf = joblib.load(r"models\tfidf_vectorizer.pkl")
+
+st.title("Aplikasi Klasifikasi Komentar Publik")
+st.write("Aplikasi ini dibuat menggunakan Teknologi NLP dengan memanfaatkan model machine learning Logistic Regression")
+input = st.text_input("Masukkan Komentar Anda!!")
+if st.button("Submit"):
+    if input.strip() == "":
+        st.warning("Komentar tidak boleh kosong.")
+    else:
+        vector = tfidf.transform([input])
+        prediksi = model.predict(vector)[0]
+        
+        label_map = {
+            0: "Negatif",
+            1: "Positif"
+            
+        }
+
+        st.subheader("Hasil Analisis Komentar")
+        st.write("**Komentar :**", label_map.get(prediksi, prediksi))
